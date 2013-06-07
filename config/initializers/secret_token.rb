ee9c716a8eadd67a93a68e2aeb128473591e7cb9
Brian Rue <brian@rollbar.com>
2013-06-07 14:57:26 -0700

    first commit (fresh rails app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuAddonRailsTest::Application.config.secret_token = '7271ef57bb59b5783df8b12a269805b88779fd60ad1781bba7a209efac2a2d19f81600bc84037fd4e51715e7891635e432d1e2be95db445e4d2ea20a92380dd3'
